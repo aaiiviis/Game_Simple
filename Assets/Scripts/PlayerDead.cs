5c3c6cba42e0ae30e0f96c9b57fd815893fc601a
aaiiviis <aivis.skangalis@gmail.com>
2020-04-24 15:54:34 +0300

    Player dead/need fix

---
+++ Assets/Scripts/PlayerDead.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDead : MonoBehaviour {
+
+public GameObject Player;
+public GameObject Ragdoll;
+
+void OnTriggerEnter(Collider other){
+ if (other.tag == "DeadZone") {
+  Player.SetActive (false);
+  Ragdoll.SetActive (true);
+  Instantiate(Ragdoll, transform.position, transform.rotation);
+ }
+}
+
+
+}
