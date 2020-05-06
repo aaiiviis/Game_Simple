8b03606103d9807a21accc198a85593fbea66099
aaiiviis <aivis.skangalis@gmail.com>
2020-05-06 12:45:27 +0300

    AI made/reach trigger follows

---
+++ Assets/Scripts/EmeryWalk.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmeryWalk : MonoBehaviour {
+
+public GameObject Emery;
+public float timer;
+public bool yes;
+public float newtimer;
+
+
+void Start () {
+	newtimer=timer;
+
+}
+
+void OnTriggerEnter(Collider other){
+	if (other.tag == "Player") {
+		Emery.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+		Emery.GetComponent<Animator>().SetTrigger ("Walk");
+		
+		
+	}
+}
+void OnTriggerExit(Collider other){
+	if(other.tag == "Player") {
+	yes=true;
+	}
+		
+	
+	
+}
+void Update () {
+	if(yes==true){
+		timer -= Time.deltaTime;  
+	}
+	if(timer < 0){
+		Emery.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+		yes=false;
+		timer=newtimer;
+		Emery.GetComponent<Animator>().SetTrigger ("Idle");
+	}
+		
+}
+}
+
+
+
+	
---
+++ Assets/Scripts/NaMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaMesh : MonoBehaviour {
+
+public Transform target;
+UnityEngine.AI.NavMeshAgent Agent;
+
+void Start (){
+	Agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	
+}
+
+
+void Update(){
+	Agent.SetDestination(target.position);
+}
+}
