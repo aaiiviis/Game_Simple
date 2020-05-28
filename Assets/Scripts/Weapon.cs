91b39d1e2597a78d5d741c0faffe99ac18ff1254
aaiiviis <aivis.skangalis@gmail.com>
2020-05-28 10:59:20 +0300

    FireBall add/Sounds add shoot reload

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,21 +10,31 @@ public class Weapon : MonoBehaviour {
 	public AudioClip Fire;
 	public AudioClip Reload;
 	public GameObject Pistol;	
+	public GameObject FireBall;
+
+	void Start(){
+		FireBall. SetActive (false);
+	}
 
 	void Update () {
 		if (Input.GetMouseButtonDown (0) & Magaz > 0) {
+			FireBall. SetActive (true);
 			Pistol.GetComponent<Animator>(). SetTrigger("Shot");
 			Transform BulletInstance = (Transform)Instantiate (bullet, GameObject.Find ("Spawn").transform.position, Quaternion.identity);
 			BulletInstance.GetComponent<Rigidbody> ().AddForce (transform.forward * BulletForce);
 			Magaz = Magaz - 1;
 			GetComponent<AudioSource> ().PlayOneShot (Fire);
-			GetComponent<AudioSource> ().PlayOneShot (Reload);
+			
 
 		}
 		if (Input.GetKeyDown (KeyCode.R)) {
 			Magaz = 7;
+			GetComponent<AudioSource> ().PlayOneShot (Reload);
 			Pistol.GetComponent<Animator>(). SetTrigger("Reload");
 		
+		}
+		if (Input.GetMouseButtonUp(0)){
+			FireBall.SetActive (false);
 		}
 	}
 }
